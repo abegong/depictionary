c6521d879e89779e9ad282db9269dca535c7d047
Abe Gong <agong@umich.edu>
2012-06-24 14:48:30 -0400

    Load the right number of words in each ajax call

--- html/depictionary.js
+++ html/depictionary.js
@@ -1,9 +1,28 @@
-//word_list : ["acres","adult","advice","arrangement","attempt","August","Autumn","border","breeze","brick","calm","canal","Casey","cast","chose","claws","coach","constantly","contrast","cookies","customs","damage","Danny","deeply","depth","discussion","doll","donkey","Egypt","Ellen","essential","exchange","exist","explanation","facing","film","finest","fireplace","floating","folks","fort","garage","grabbed","grandmother","habit","happily","Harry","heading","hunter","Illinois","image","independent","instant","January","kids","label","Lee","lungs","manufacturing","Martin","mathematics","melted","memory","mill","mission","monkey","Mount","mysterious","neighborhood","Norway","nuts","occasionally","official","ourselves","palace","Pennsylvania","Philadelphia","plates","poetry","policeman","positive","possibly","practical","pride","promised","recall","relationship","remarkable","require","rhyme","rocky","rubbed","rush","sale","satellites","satisfied","scared","selection","shake","shaking","shallow","shout","silly","simplest","slight","slip","slope","soap","solar","species","spin","stiff","swung","tales","thumb","tobacco","toy","trap","treated","tune","University","vapor","vessels","wealth","wolf","zoo"],
+$("#playff").live("pageinit", function() {
+    timer = 6000;
+
+    $("#guess-words li").each(function(i,w){
+        set_word(i,w);
+    });
+
+    $("#guess-words li").click(function(){
+        set_word(2,this);
+    });
+
+    game_time = 599;
+    $("#game-time").html( Math.floor(game_time/10)+"."+(game_time%10) );
+
+    $("#guess-words li").swipe(function(){
+        $(this).hide();
+    });
+
+    setTimeout(countdown, 100);
+});
 
 
 var DPManager = {
     _words : [],
-    _create : {},
+    _time : 0,
 
     next_word : function(){
         return(word_list[Math.floor(Math.random()*word_list.length)]);
@@ -16,13 +35,24 @@ var DPManager = {
             .fadeIn(200*t);
     },
 
+    countdown : function(){
+        timer -= 100;
+        $("#game-time").html( (timer/1000).toFixed(1) );
+        if( timer > 0 ){
+            setTimeout(countdown, 100);
+        }
+        else{
+            //The game is now over
+        }
+    },
+
     setWords : function(data){
         console.log(data);
         DPManager._words = data.words;
     },
 
     startGame : function(){
-//        location.hash = 'countdown';
+        DPManager._time = 6000;
         $.mobile.changePage('/#countdown');
     },
     pauseGame : function(){},
@@ -37,6 +67,7 @@ var DPManager = {
     },
 
     initPlayPage : function(){
+        setTimeout(DPManager.countdown, 100);
         console.log("Here we are");
         console.log(DPManager._words);
     }
